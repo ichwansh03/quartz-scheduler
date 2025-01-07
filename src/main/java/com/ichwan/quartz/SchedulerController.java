e6cfb091bc83ac111f7267a9bb5afa9c500ff795
ichwansh03 <ichwansholihin70@gmail.com>
2025-01-08 06:36:31 +0700

    add spesific moment scheduled

--- src/main/java/com/ichwan/quartz/SchedulerController.java
+++ src/main/java/com/ichwan/quartz/SchedulerController.java
@@ -4,6 +4,9 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.DateBuilder.*;
 
 @RestController
 public class SchedulerController {
@@ -26,6 +29,32 @@ public class SchedulerController {
                 .build();
 
         scheduler.scheduleJob(job, trigger);
+
+        return "Done";
+    }
+
+    //spesific moment in time, then repeating every 10 seconds 10 times
+    @GetMapping("/spesific")
+    public String scheduledSpesific() throws SchedulerException {
+
+        SchedulerFactory sch = new StdSchedulerFactory();
+        Scheduler scheduler = sch.getScheduler();
+
+        scheduler.start();
+
+        JobDetail job = JobBuilder.newJob(SimpleJob.class)
+                .usingJobData("name", "Abdul Ali")
+                .build();
+
+        Trigger trigger = newTrigger()
+                .withIdentity("myTrigger1","group1")
+                .withSchedule(simpleSchedule()
+                        .withIntervalInSeconds(10)
+                        .withRepeatCount(10))
+                .forJob(job)
+                .build();
+
+        scheduler.scheduleJob(trigger);
 
         return "Done";
     }
